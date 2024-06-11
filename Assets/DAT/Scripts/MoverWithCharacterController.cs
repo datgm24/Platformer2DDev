97c71a1595fd3a9968b254d81e76ef04de957b18
YuTanaka <tanakaedu@users.noreply.github.com>
2024-06-11 14:04:38 +0900

    段差と、落差の実装(WIP)
    - 高い段差につっこむと、いどうできなくなる

--- Assets/DAT/Scripts/MoverWithCharacterController.cs
+++ Assets/DAT/Scripts/MoverWithCharacterController.cs
@@ -23,11 +23,15 @@ namespace DAT
 
         float walkDirection = 0;
         bool isJumped = false;
+        BoxCollider2D boxCollider;
+        int collideLayers;
 
         void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             fallable = GetComponent<IFallable>();
+            boxCollider = GetComponent<BoxCollider2D>();
+            collideLayers = LayerMask.GetMask("Floor", "GimmickCollision");
         }
 
         public void Jump()
@@ -69,10 +73,89 @@ namespace DAT
             }
 
             // 移動量を求める
-            var tickDir = Time.deltaTime * walkSpeed * walkDirection * moveDir;
+            Vector2 tickMove = Time.deltaTime * walkSpeed * walkDirection * moveDir;
 
-            // TODO: 接触の判定
-            rb.position += tickDir;
+            // 水平方向の接触の判定
+            tickMove = CheckTickMove(tickMove);
+
+            // 確定した移動を反映
+            rb.position += tickMove;
+        }
+
+        /// <summary>
+        /// 予定の方向へ移動できるかを確認して、
+        /// 移動できる移動量を返す。
+        /// </summary>
+        /// <param name="tickMove">移動したいベクトル</param>
+        /// <returns>移動可能なベクトル</returns>
+        Vector2 CheckTickMove(Vector2 tickMove)
+        {
+            if (Mathf.Approximately(tickMove.magnitude, 0))
+            {
+                return tickMove;
+            }
+
+            int count = CharacterCaster.Cast(
+                rb.position + boxCollider.offset,
+                boxCollider, tickMove,
+                collideLayers);
+
+            // 接触がなければ、下方向の段差チェック
+            if (count == 0)
+            {
+                return CheckDownStep(tickMove);
+            }
+            // 接触があれば、上方向の段差チェック
+            return CheckUpStep(tickMove);
+        }
+
+        /// <summary>
+        /// 下りの段差チェック
+        /// </summary>
+        /// <param name="tickMove">移動したいベクトル</param>
+        /// <returns>移動させるベクトル</returns>
+        Vector2 CheckDownStep(Vector2 tickMove)
+        {
+            // 移動後の座標から、下方向の段差を確認
+            Vector2 nextPos = rb.position + tickMove;
+
+            // 下方向に段差チェック
+            int count = CharacterCaster.Cast(
+                rb.position+boxCollider.offset,
+                boxCollider,
+                step * Vector2.down,
+                collideLayers);
+
+            if (count == 0)
+            {
+                // 足が下せない段差なので、そのまま飛ぶ
+                return tickMove;
+            }
+
+            // 足が届くので、移動量を調整する
+            tickMove.y = -CharacterCaster.GetNearestRaycastHit2D().Value.distance;
+            return tickMove;
+        }
+
+        /// <summary>
+        /// 登りの段差チェック
+        /// </summary>
+        /// <param name="tickMove">移動したいベクトル</param>
+        /// <returns>移動させるベクトル</returns>
+        Vector2 CheckUpStep(Vector2 tickMove)
+        {
+            // 移動量を調整
+            float nextStep = CharacterCaster.GetStepHeight();
+            if (nextStep <= step)
+            {
+                // 乗り越えられる
+                tickMove.y = nextStep;
+                return tickMove;
+            }
+
+            // 乗り越えられない
+            tickMove.x = Mathf.Sign(tickMove.x) * CharacterCaster.GetNearestRaycastHit2D().Value.distance;
+            return tickMove;
         }
     }
 }
